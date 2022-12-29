6a4e3e288ac9cf6c1241af366c9c75c2df66733c
alissonccs <alisson.css2@gmail.com>
2022-12-29 14:47:14 -0300

    tp_rnd_ef implementation

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -21,6 +21,7 @@ RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_1_08_03_2022_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_OMEGA_DATA_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_06_07_2022_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_29_12_2022_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_09_03_2022_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_23_07_2022_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_FRAT_31_07_2022_mod();
@@ -42,6 +43,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_mod, 0},
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_OMEGA_DATA_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_04_07_2022_OMEGA_DATA_mod, 0},
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_06_07_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_06_07_2022_mod, 0},
+    {"_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_29_12_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_FRAT_29_12_2022_mod, 0},
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_09_03_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_09_03_2022_mod, 0},
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_23_07_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_23_07_2022_mod, 0},
     {"_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_FRAT_31_07_2022_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4NHPP_ZI_LOGISTCOV_FRAT_31_07_2022_mod, 0},

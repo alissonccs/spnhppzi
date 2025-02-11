cfa7e6704fae136fc36c6109ff47d4731d83c67b
alissonccs <82158195+alissonccs@users.noreply.github.com>
2025-02-10 22:50:38 -0300

    adiciona exemplo

--- R/spnhppzi-package.R
+++ R/spnhppzi-package.R
@@ -1,14 +1,19 @@
 #' The 'spnhppzi' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
+#' @title spnhppzi: Hierarchical Models for Recurrent Event Data with Zero Inflation and Spatial Correlation
+#' @description Provides statistical models for analyzing recurrent event data with zero inflation and spatial correlation.
 #'
-#' _PACKAGE
+#' @details
+#' The `spnhppzi` package provides tools for modeling recurrent event data, including spatially correlated zero-inflated models.
+#'
 #' @name spnhppzi-package
 #' @aliases spnhppzi
 #' @useDynLib spnhppzi, .registration = TRUE
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
+#' @importFrom stats formula
+#' @importFrom utils read.table write.table
 #'
 #' @references
 #' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.5. https://mc-stan.org
